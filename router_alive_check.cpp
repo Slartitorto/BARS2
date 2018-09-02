d752244ba4b254e18fa839837c57d9e538c0eae4
Slartitorto <ivano.tortolini@gmail.com>
2018-09-02 17:38:33 +0200

    Added logfile

--- router_alive_check.cpp
+++ router_alive_check.cpp
@@ -1,4 +1,4 @@
-// g++ `mysql_config --cflags` `mysql_config --libs` -o router_alive_check router_alive_check.c
+// g++ `mysql_config --cflags` `mysql_config --libs` -o hoolyrouter_alive_check hoolyrouter_alive_check.c
 #include <cstdlib>
 #include <stdio.h>
 #include <string.h>
@@ -12,15 +12,22 @@
 #define DATABASE_USERNAME "hooly"
 #define DATABASE_PASSWORD "hooly_pwd"
 
+FILE *logfile = fopen("/var/log/hoolyrouter_alive_check.log","a");
+
 int main(void)
 {
+  time_t t = time(NULL);
+  struct tm *tm = localtime(&t);
+  fprintf(logfile,"%s\n", asctime(tm));
+  fprintf(logfile,"Starting ...\n");
+  fflush(logfile);
   //initialize MYSQL object for connections
   MYSQL *mysql_conn =  mysql_init(NULL);
   // *** Connect to the database
   if (mysql_real_connect(mysql_conn, DATABASE_SERVER, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME, 0, NULL, 0)) {
-    fprintf(stdout,"Connesso al database\n");
+    fprintf(logfile,"Connesso al database\n");
   } else {
-    fprintf(stdout,"Errore di connessione al database\n");
+    fprintf(logfile,"Errore di connessione al database\n");
     exit(0);
   }
 
@@ -52,32 +59,34 @@ int main(void)
   int MYSQL_NUM_RES;
   MYSQL_ROW row;
 
-  time_t t = time(NULL);
-  struct tm *tm = localtime(&t);
-  fprintf(stdout,"%s\n", asctime(tm));
+  t = time(NULL);
+  tm = localtime(&t);
+  fprintf(logfile,"%s\n", asctime(tm));
+  fflush(logfile);
   while(1) {
     sprintf(query, "SELECT router, timestamp from keep_alive_check where alarmed = 0 and timestamp < now() - interval 150 second");
-    fprintf(stdout,".");
+    fprintf(logfile,".");
+    fflush(logfile);
     if (counter == 100) {
-      fprintf(stdout,"\n");
+      fprintf(logfile,"\n");
       time_t t = time(NULL);
       struct tm *tm = localtime(&t);
-      fprintf(stdout,"%s", asctime(tm));
+      fprintf(logfile,"%s", asctime(tm));
 
       counter = 0;
     }
-    fflush(stdout);
     mysql_query(mysql_conn,query);
     MYSQL_RES *result = mysql_store_result(mysql_conn);
     MYSQL_NUM_RES = mysql_num_rows(result);
 
     if(MYSQL_NUM_RES > 0) {
-      fprintf(stdout,"\nRilevato router in timeout\n");
+      fprintf(logfile,"\nRilevato router in timeout\n");
       int x = 0;  // x = vettore numero di router, potrebbero essercene diversi
       while ((row = mysql_fetch_row(result))) {
         router[x] = row[0];
         timestamp[x] = row[1];
-        fprintf(stdout,"timestamp: %s - router: %s\n",timestamp[x],router[x]);
+        fprintf(logfile,"timestamp: %s - router: %s\n",timestamp[x],router[x]);
+        fflush(logfile);
         x++;
       }
       mysql_free_result(result);
@@ -85,11 +94,11 @@ int main(void)
       for (int a=0; a<x; a++) { // per ciascun router x, a = altro vettore al router
         // metti a 1 il flag "alarmed" nella tabella keep_alive_check
         sprintf(query, "UPDATE keep_alive_check set alarmed = 1 where router = '%s'",router[a]);
-        fprintf(stdout,"update db: query = %s \n",query);
+        fprintf(logfile,"update db: query = %s \n",query);
         mysql_query(mysql_conn,query);
 
         sprintf(query,"select codUtente from router where router = '%s'",router[a]);
-        fprintf(stdout,"query = %s\n",query);
+        fprintf(logfile,"query = %s\n",query);
         mysql_query(mysql_conn,query);
         MYSQL_RES *result = mysql_store_result(mysql_conn);
         MYSQL_NUM_RES = mysql_num_rows(result);
@@ -99,9 +108,9 @@ int main(void)
           }
           mysql_free_result(result);
 
-          fprintf(stdout,"Notifica a codUtente %s\n",codUtente);
+          fprintf(logfile,"Notifica a codUtente %s\n",codUtente);
           sprintf(query,"select telegram_flag,telegram_chatid,pushbullett_flag,pushbullett_addr,email_flag,email_addr,whatsapp_flag,whatsapp_tel,sms_flag,sms_tel from notify_method where codUtente = '%s'",codUtente);
-          fprintf(stdout,"query = %s\n",query);
+          fprintf(logfile,"query = %s\n",query);
 
           mysql_query(mysql_conn,query);
           MYSQL_RES *result = mysql_store_result(mysql_conn);
@@ -119,22 +128,23 @@ int main(void)
               sms_flag = row[8];
               sms_tel = row[9];
             }
-            fprintf(stdout,"telegram_flag = %s\n",telegram_flag);
-            fprintf(stdout,"telegram_chatid = %s\n",telegram_chatid);
-            fprintf(stdout,"pushbullett_flag = %s\n",pushbullett_flag);
-            fprintf(stdout,"pushbullett_addr = %s\n",pushbullett_addr);
-            fprintf(stdout,"email_flag = %s\n",email_flag);
-            fprintf(stdout,"email_addr = %s\n",email_addr);
-            fprintf(stdout,"whatsapp_flag = %s\n",whatsapp_flag);
-            fprintf(stdout,"whatsapp_tel = %s\n",whatsapp_tel);
-            fprintf(stdout,"sms_flag = %s\n",sms_flag);
-            fprintf(stdout,"sms_tel = %s\n",sms_tel);
+            fprintf(logfile,"telegram_flag = %s\n",telegram_flag);
+            fprintf(logfile,"telegram_chatid = %s\n",telegram_chatid);
+            fprintf(logfile,"pushbullett_flag = %s\n",pushbullett_flag);
+            fprintf(logfile,"pushbullett_addr = %s\n",pushbullett_addr);
+            fprintf(logfile,"email_flag = %s\n",email_flag);
+            fprintf(logfile,"email_addr = %s\n",email_addr);
+            fprintf(logfile,"whatsapp_flag = %s\n",whatsapp_flag);
+            fprintf(logfile,"whatsapp_tel = %s\n",whatsapp_tel);
+            fprintf(logfile,"sms_flag = %s\n",sms_flag);
+            fprintf(logfile,"sms_tel = %s\n",sms_tel);
+            fflush(logfile);
 
             if(atoi(telegram_flag) == 1) {
-              fprintf(stdout,"Attivato allarme telegram = %s\n",telegram_flag);
-              fprintf(stdout," telegram_chatid = %s\n",telegram_chatid);
+              fprintf(logfile,"Attivato allarme telegram = %s\n",telegram_flag);
+              fprintf(logfile," telegram_chatid = %s\n",telegram_chatid);
               sprintf(query,"SELECT telegram_BOT_ID from server_settings");
-              fprintf(stdout,"query = %s\n",query);
+              fprintf(logfile,"query = %s\n",query);
               mysql_query(mysql_conn,query);
               MYSQL_RES *result = mysql_store_result(mysql_conn);
               MYSQL_NUM_RES = mysql_num_rows(result);
@@ -146,15 +156,16 @@ int main(void)
               }
               mysql_free_result(result);
               sprintf(command,"wget -q -O/dev/null --no-cache --spider \"https://api.telegram.org/%s/sendMessage?chat_id=%s&text=%s-%s\"",telegram_BOT_ID,telegram_chatid,subject,message);
-              fprintf(stdout,"%s\n",command);
+              fprintf(logfile,"%s\n",command);
+              fflush(logfile);
               system(command);
             }
 
             if(atoi(pushbullett_flag) == 1) {
-              fprintf(stdout,"Attivato allarme pushbullett = %s\n",pushbullett_flag);
-              fprintf(stdout," pushbullett_addr = %s\n",pushbullett_addr);
+              fprintf(logfile,"Attivato allarme pushbullett = %s\n",pushbullett_flag);
+              fprintf(logfile," pushbullett_addr = %s\n",pushbullett_addr);
               sprintf(query,"SELECT pushbullett_token,sendmessage_key from server_settings");
-              fprintf(stdout,"query = %s\n",query);
+              fprintf(logfile,"query = %s\n",query);
               mysql_query(mysql_conn,query);
               MYSQL_RES *result = mysql_store_result(mysql_conn);
               MYSQL_NUM_RES = mysql_num_rows(result);
@@ -167,26 +178,28 @@ int main(void)
               }
               mysql_free_result(result);
               sprintf(command,"wget --no-cache --spider \"http://myhooly.hooly.eu/sendmessage.php?destination=%s&channel=pushbullett&key=%s&subject=%s&message=%s\"",pushbullett_addr,sendmessage_key,subject,message);
-              fprintf(stdout,"%s\n",command);
+              fprintf(logfile,"%s\n",command);
+              fflush(logfile);
               system(command);
             }
 
             if(atoi(email_flag) == 1) {
-              fprintf(stdout,"attivato allarme email = %s con email_addr = %s\n",email_flag,email_addr);
-              sprintf(command,"echo \"Allarme da router %s; non ricevo dati da 2 min.\"|mail -r hooly@hooly.eu -s \"Allarme di collegamento router %s\" %s",router[x],router[x],email_addr);
-              fprintf(stdout,"send mail as: %s \n\r",command);
+              fprintf(logfile,"attivato allarme email = %s con email_addr = %s\n",email_flag,email_addr);
+              sprintf(command,"echo \"Allarme da router %s; non ricevo dati da 2 min.\"|mail -r hooly@hooly.eu -s \"Allarme di collegamento router %s\" %s",router[a],router[a],email_addr);
+              fprintf(logfile,"send mail as: %s \n\r",command);
+              fflush(logfile);
               system(command);
             }
 
             if(atoi(whatsapp_flag) == 1) {
-              fprintf(stdout,"attivato allarme whatsapp = %s con whatsapp_tel = %s\n",whatsapp_flag,whatsapp_tel);
+              fprintf(logfile,"attivato allarme whatsapp = %s con whatsapp_tel = %s\n",whatsapp_flag,whatsapp_tel);
             }
 
             if(atoi(sms_flag) == 1) {
-              fprintf(stdout,"Attivato allarme sms = %s\n",sms_flag);
-              fprintf(stdout," sms_tel = %s\n",sms_tel);
+              fprintf(logfile,"Attivato allarme sms = %s\n",sms_flag);
+              fprintf(logfile," sms_tel = %s\n",sms_tel);
               sprintf(query,"SELECT sendmessage_key from server_settings");
-              fprintf(stdout,"query = %s\n",query);
+              fprintf(logfile,"query = %s\n",query);
               mysql_query(mysql_conn,query);
               MYSQL_RES *result = mysql_store_result(mysql_conn);
               MYSQL_NUM_RES = mysql_num_rows(result);
@@ -197,7 +210,8 @@ int main(void)
               }
               mysql_free_result(result);
               sprintf(command,"wget --no-cache --spider \"http://myhooly.hooly.eu/sendmessage.php?destination=%s&channel=sms&key=%s&subject=%s&message=%s\"",sms_tel,sendmessage_key,subject,message);
-              fprintf(stdout,"%s\n",command);
+              fprintf(logfile,"%s\n",command);
+              fflush(logfile);
               system(command);
             }
           }
